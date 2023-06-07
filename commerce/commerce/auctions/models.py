f75c0cdb8799d262f2a391bae047b90edcf51494
Julia Maxy <jjuliamaxxx@gmail.com>
2023-06-07 11:23:18 +0300

    create bid model

--- commerce/commerce/auctions/models.py
+++ commerce/commerce/auctions/models.py
@@ -5,6 +5,15 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+class Bid(models.Model):
+    user =  models.ManyToManyField(User,related_name="bids")
+    listing = models.ForeignKey("Listing", on_delete=models.CASCADE, related_name="bids")
+    added_time = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return f"{self.listing}-{self.amount}"
+
+
 class WatchList(models.Model):
     user = models.ManyToManyField(User,related_name="watchlist")
     listing = models.ForeignKey("Listing", on_delete=models.CASCADE, related_name="watchlisted")
@@ -22,7 +31,8 @@ class Listing(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
-    starting_bid = models.IntegerField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    active = models.BooleanField(default=True)
     picture = models.ImageField(null=True, blank=True, upload_to='images/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings")
     condition = models.CharField(max_length=100)
